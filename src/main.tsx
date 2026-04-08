fce6282489af8564b6de96149cd2915221693edb
TOMYI-0881 <gthomasenrique0882@gmail.com>
2026-04-08 15:13:34 -0500

    cambios

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { Button } from "./components/ui/button";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <h1>HOLA MUNDO</h1>
+    <h1 className="flex flex-col items-center">HOLA MUNDO</h1>
+    <Button>Hola desde aca</Button>
   </StrictMode>,
 );
